2ad77a34c1522996ccacc94a5125f1d0536e98cd
adonozo <ar.donozo@acad.ucb.edu.bo>
2021-07-16 00:43:00 +0100

    Add patients table

--- src/app/pages/patients/patients.component.ts
+++ src/app/pages/patients/patients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PatientsService} from "../../@core/services/patients.service";
+import {LocalDataSource} from "ng2-smart-table";
 
 @Component({
   selector: 'app-patients',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patients.component.scss']
 })
 export class PatientsComponent implements OnInit {
+  source: LocalDataSource;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  settings = {
+    columns: {
+      name: {
+        title: 'Name',
+        type: 'string'
+      },
+      email: {
+        title: 'Email',
+        type: 'number'
+      }
+    },
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+      custom: [
+        {
+          name: 'new-treatment',
+          title: '<i class="nb-plus-circled inline-block width: 50px"></i>'
+        },
+        {
+          name: 'records',
+          title: '<i class="nb-list inline-block width: 50px"></i>',
+        }
+      ]
+    }
   }
 
+  constructor(private patientService: PatientsService) { }
+
+  ngOnInit(): void {
+    this.getPatientData();
+  }
+
+  private getPatientData(): void {
+    this.patientService.getPatientList()
+      .subscribe(patients =>
+      {
+        this.source = new LocalDataSource(patients);
+      })
+  }
 }
