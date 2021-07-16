cf35537082d1ea5fc04ef9fabebbb12cbdf46c2e
adonozo <ar.donozo@acad.ucb.edu.bo>
2021-07-16 12:51:40 +0100

    Create new treatment component

---
+++ src/app/pages/patients/new-treatment/new-treatment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-treatment',
+  templateUrl: './new-treatment.component.html',
+  styleUrls: ['./new-treatment.component.scss']
+})
+export class NewTreatmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
