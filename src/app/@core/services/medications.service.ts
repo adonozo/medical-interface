6f157421c03510cfab380cb642c6d121faa1457e
adonozo <ar.donozo@acad.ucb.edu.bo>
2021-08-17 20:05:44 +0100

    Get the medication list from the rest api

--- src/app/@core/services/medications.service.ts
+++ src/app/@core/services/medications.service.ts
@@ -1,61 +1,23 @@
 import { Injectable } from '@angular/core';
 import {RestApiService} from "./rest-api.service";
-import {Medication, Quantity} from "fhir/r4";
-import {from, Observable} from "rxjs";
+import {Bundle, Medication, Quantity} from "fhir/r4";
+import {Observable} from "rxjs";
 import {MedicationQuantities} from "./data/medication-quantities";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicationsService {
-  private medications: Medication[] = [{
-    "resourceType": "Medication",
-    "id": "60fb0a79c055e8c0d3f853d0",
-    "code": {
-      "coding": [
-        {
-          "system": "http://hl7.org/fhir/sid/ndc",
-          "code": "0169-7501-11",
-          "display": "Novolog 100u/ml"
-        }
-      ]
-    },
-    "form": {
-      "coding": [
-        {
-          "system": "http://snomed.info/sct",
-          "code": "385219001",
-          "display": "Injection solution (qualifier value)"
-        }
-      ]
-    },
-  },{
-    "resourceType": "Medication",
-    "id": "60fb0a79c055e8c0d3f853a5",
-    "code": {
-      "coding": [
-        {
-          "system": "http://snomed.info/sct",
-          "code": "408050008",
-          "display": "Ramipril 2.5mg tablets"
-        }
-      ]
-    },
-    "form": {
-      "coding": [
-        {
-          "system": "http://snomed.info/sct",
-          "code": "385055001",
-          "display": " Tablet"
-        }
-      ]
-    },
-  }];
+  private readonly path = 'medications/';
 
-  constructor(private restApiService: RestApiService) { }
+  constructor(private restService: RestApiService) { }
 
   public getMedications(): Observable<Medication[]> {
-    return from(new Promise<Medication[]>(resolve => resolve(this.medications)));
+    return this.restService.get<Bundle>(this.path)
+      .pipe(
+        map(bundle => bundle.entry.map(entry => entry.resource as Medication))
+      );
   }
 
   public getMedicationQuantities = (): Quantity[] => MedicationQuantities;
--- src/app/@core/services/treatments.service.ts
+++ src/app/@core/services/treatments.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {RestApiService} from "./rest-api.service";
-import {from, Observable} from "rxjs";
-import {Treatment} from "../models/treatment";
+import {Observable} from "rxjs";
 import {Bundle} from "fhir/r4";
 
 @Injectable({
@@ -13,92 +12,5 @@ export class TreatmentsService {
 
   public getTreatmentsFor(patientId: string) : Observable<Bundle> {
     return this.restService.get(`patients/${patientId}/carePlans/`);
-  }
-
-  getTreatmentsForPatient(): Observable<Treatment[]> {
-    const treatments: Treatment[] = [
-      {
-        id: '1234',
-        intent: 'plan',
-        subject: {
-          id: '12344',
-          display: 'John Doe'
-        },
-        period: {
-          start: '2021-07-01',
-          end: '2021-08-01'
-        },
-        resourceType: 'CarePlan',
-        created: '2021-07-01',
-        contained: [
-          {
-            resourceType: 'Medication',
-            status: 'active',
-            code: {
-              coding: [
-                {
-                  system: 'http://snomed.info/sct',
-                  code: '12086701000001103',
-                  display: 'Invokana (product)'
-                }
-              ]
-            }
-          }
-        ],
-        status: 'active',
-        activity: [
-          {
-            reference: {
-              id: "3ca91535-8f78-4bc8-b8ca-f95b21d23c8c",
-              type: "MedicationRequest",
-              reference: "medicationRequest/3ca91535-8f78-4bc8-b8ca-f95b21d23c8c"
-            },
-            detail: {
-              kind: 'MedicationRequest',
-              status: 'scheduled',
-            }
-          }
-        ],
-      },
-      {
-        id: '1233',
-        intent: 'plan',
-        subject: {
-          id: '12344',
-          display: 'John Doe'
-        },
-        period: {
-          start: '2021-06-01',
-          end: '2021-06-30'
-        },
-        resourceType: 'CarePlan',
-        created: '2021-06-01',
-        contained: [
-          {
-            resourceType: 'Medication',
-            status: 'active',
-            code: {
-              coding: [
-                {
-                  system: 'http://snomed.info/sct',
-                  code: '384978002',
-                  display: 'Glyburide'
-                }
-              ]
-            }
-          }
-        ],
-        status: 'completed',
-        activity: [
-          {
-            detail: {
-              kind: 'MedicationRequest',
-              status: 'completed'
-            },
-          }
-        ],
-      }
-    ];
-    return from(new Promise<Treatment[]>(resolve => resolve(treatments)));
   }
 }
