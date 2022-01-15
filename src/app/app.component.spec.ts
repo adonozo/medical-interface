86102b79ec7ecef06e0d17564f5047690748d92b
adonozo <donozo.alvaro@gmail.com>
2022-01-15 18:10:07 +0000

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('doctor-interface app is running!');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
