702602cc246371ea260d36ae62d7d595c45164d1
adonozo <ar.donozo@acad.ucb.edu.bo>
2022-02-06 00:04:44 +0000

    Add treatments and glucose levels components to patient view

--- src/app/pages/patients/patient-view/patient-view.component.ts
+++ src/app/pages/patients/patient-view/patient-view.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Patient } from "../../../@core/models/patient";
+import { PatientsService } from "../../../@core/services/patients.service";
+import { ActivatedRoute } from "@angular/router";
+import { flatMap } from "rxjs/internal/operators";
 
 @Component({
   selector: 'app-patient-view',
   templateUrl: './patient-view.component.html',
   styleUrls: ['./patient-view.component.scss']
 })
-export class PatientViewComponent implements OnInit {
+export class PatientViewComponent {
+  patient: Patient;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private patientsService: PatientsService,
+    private route: ActivatedRoute,) {
+    this.route.params.pipe(
+      flatMap(params => patientsService.getSinglePatient(params["patientId"]))
+    ).subscribe(patient => this.patient = patient);
   }
-
 }
