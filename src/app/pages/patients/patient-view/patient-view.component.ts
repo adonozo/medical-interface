738aab6d408a8d361f2e04f2b470315dd47c5931
adonozo <donozo.alvaro@gmail.com>
2022-02-08 19:38:36 +0000

    Add buttons to create medication and blood glucose orders

--- src/app/pages/patients/patient-view/patient-view.component.ts
+++ src/app/pages/patients/patient-view/patient-view.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Patient } from "../../../@core/models/patient";
 import { PatientsService } from "../../../@core/services/patients.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { flatMap } from "rxjs/internal/operators";
 
 @Component({
@@ -11,12 +11,29 @@ import { flatMap } from "rxjs/internal/operators";
 })
 export class PatientViewComponent {
   patient: Patient;
+  patientId: string;
 
   constructor(
     private patientsService: PatientsService,
-    private route: ActivatedRoute,) {
+    private activatedRoute: ActivatedRoute,
+    private route: ActivatedRoute,
+    private router: Router) {
     this.route.params.pipe(
-      flatMap(params => patientsService.getSinglePatient(params["patientId"]))
+      flatMap(params => {
+        this.patientId = params["patientId"];
+        return patientsService.getSinglePatient(params["patientId"]);
+      })
     ).subscribe(patient => this.patient = patient);
+  }
+
+  public async navigate(page: string): Promise<void> {
+    switch (page) {
+      case 'medication':
+        await this.router.navigate([this.patientId + '/new-medication-request'], {relativeTo: this.activatedRoute.parent});
+        break;
+      case 'service':
+        await this.router.navigate([this.patientId + '/new-service-request'], {relativeTo: this.activatedRoute.parent});
+        return;
+    }
   }
 }
