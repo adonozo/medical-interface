ad18d46c4ed30523bb0633d0aa37413a6c3ad9b2
adonozo <donozo.alvaro@gmail.com>
2022-05-15 18:45:33 +0100

    Extend observation-form.component from form-component

--- src/app/pages/patients/observation-form/observation-form.component.ts
+++ src/app/pages/patients/observation-form/observation-form.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { NbDialogRef } from "@nebular/theme";
 import { TimeOfDay } from "../medication-request-form/form-data";
+import { FormComponent } from "../../../@core/components/form.component";
 
 @Component({
   selector: 'app-observation-form',
   templateUrl: './observation-form.component.html',
   styleUrls: ['./observation-form.component.scss']
 })
-export class ObservationFormComponent {
+export class ObservationFormComponent extends FormComponent {
   timesOfDay = TimeOfDay;
 
   constructor(
     private dialogRef: NbDialogRef<any>
   ) {
+    super();
   }
 
   close(): void {
     this.dialogRef.close();
+  }
+
+  submitForm(): void {
   }
 }
