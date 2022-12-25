c18167500a2900da577875b5452511c8f356848b
adonozo <donozo.alvaro@gmail.com>
2022-12-25 21:06:38 +0000

    Add care plan service

---
+++ src/app/@core/services/care-plan.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { RestApiService } from "./rest-api.service";
+import { Observable } from "rxjs";
+import { PaginatedResult } from "../models/paginatedResult";
+import { CarePlan } from "fhir/r4";
+import { ResourceUtils } from "./utils/resourceUtils";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarePlanService {
+  private readonly path = 'carePlans/'
+
+  constructor(private restService: RestApiService) {
+  }
+
+  getCarePlans(patientId: string, limit: number = 0, lastCursor?: string): Observable<PaginatedResult<CarePlan>> {
+    return this.restService.getPaginated(`patients/${patientId}/carePlans`, limit, lastCursor);
+  }
+
+  getCarePlan(id: string): Observable<CarePlan> {
+    return this.restService.get(this.path + id);
+  }
+
+  createCarePlan(patientId: string): Observable<CarePlan> {
+    const carePlan: CarePlan = {
+      resourceType: 'CarePlan',
+      intent: 'plan',
+      status: 'draft',
+      subject: {
+        reference: ResourceUtils.getPatientReference(patientId),
+      }
+    }
+    return this.restService.post(this.path, carePlan);
+  }
+}
