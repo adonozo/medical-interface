30d4ad9c99674adaafa2a4f34cba1459bc4d48a9
adonozo <donozo.alvaro@gmail.com>
2023-05-01 00:13:38 +0100

    Add edit button to service-request-view

--- src/app/pages/patients/components/service-request-view/service-request-view.component.ts
+++ src/app/pages/patients/components/service-request-view/service-request-view.component.ts
@@ -3,6 +3,7 @@ import { ServiceRequestView } from "../../../../@core/models/service-request-vie
 import { DayCode } from "../../../../@core/models/types";
 import * as utils from "../../../../@core/services/utils/utils";
 import { timingToString } from "../../../../@core/services/utils/utils";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-service-request-view',
@@ -12,11 +13,23 @@ import { timingToString } from "../../../../@core/services/utils/utils";
 export class ServiceRequestViewComponent {
 
   @Input() serviceRequestView: ServiceRequestView;
-  @Input() edit: Boolean;
+  @Input() editButton: Boolean;
+  @Input() disableEditButton: Boolean;
+  @Input() editPageRoute: string;
+
+  constructor(
+    protected router: Router,
+    protected activatedRoute: ActivatedRoute
+  ) {
+  }
 
   dayStringFromCode = (dayCode: DayCode): string => utils.dayStringFromCode(dayCode);
 
   whenArrayToString = (when: string[]): string => when
     .map(whenCode => timingToString(whenCode))
     .join(', ');
+
+  navigateToEdit = async () => {
+    await this.router.navigate([this.editPageRoute], {relativeTo: this.activatedRoute.parent})
+  }
 }
