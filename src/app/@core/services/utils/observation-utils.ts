b267171c1a3497338a3558b6a213a99e730fcc76
adonozo <ar.donozo@acad.ucb.edu.bo>
2024-02-23 14:39:06 -0400

    Add function comment

--- src/app/@core/services/utils/observation-utils.ts
+++ src/app/@core/services/utils/observation-utils.ts
@@ -1,5 +1,9 @@
 import { Observation } from "fhir/r5";
 
+/**
+ * Gets the observation value (value + unit) as a human-friendly string
+ * @param observation The observation to get the value from
+ */
 export function getDisplayValue(observation: Observation): string {
   return `${observation.valueQuantity?.value} ${observation.valueQuantity?.unit}`;
 }
